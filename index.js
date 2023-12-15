e6be1e6c2fa1a27bb9ecdd1129a8ba02bfd3d0db
LesHolikov <sanyagolikov97@gmail.com>
2023-12-15 14:15:04 +0000

    Set deployed host to tg bot web_app

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const TelegramBot = require("node-telegram-bot-api");
 require("dotenv").config();
 
 const token = process.env.BOT_TOKEN;
+const client = process.env.CLIENT;
+
 const bot = new TelegramBot(token, { polling: true });
 
-bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, "Привіт! Як тебе звати?").then(() => {
-    bot.once("message", (msg) => {
-      const chatId = msg.chat.id;
-      const userName = msg.text.replace("/start", ""); // Вилучення команди /start з повідомлення
-      bot.sendMessage(
-        chatId,
-        `Приємно познайомитися, ${userName.trim()}! Вітаю тебе!`
-      );
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  if (text === "/start") {
+    await bot.sendMessage(chatId, "Welcome to secret shop!", {
+      reply_markup: {
+        keyboard: [[{ text: "Get started!", web_app: { url: client } }]],
+      },
     });
-  });
+  }
 });

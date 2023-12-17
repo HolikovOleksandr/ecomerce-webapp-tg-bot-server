ea928d313a1b849e3eeee10e9f4df3bbc8ffc662
LesHolikov <sanyagolikov97@gmail.com>
2023-12-17 14:05:02 +0000

    save changes

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const TelegramBot = require("node-telegram-bot-api");
 require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
 const token = process.env.BOT_TOKEN;
 const client = process.env.CLIENT;
 const helloImg = process.env.HELLO_IMG;
 
 const bot = new TelegramBot(token, { polling: true });
+const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
@@ -16,7 +22,7 @@ bot.on("message", async (msg) => {
       caption: "Welcome to secret shop!",
       reply_markup: {
         keyboard: [
-          [{ text: "Set form data!", web_app: { url: client + "/form" } }],
+          [{ text: "Input data!", web_app: { url: client + "/form" } }],
         ],
       },
     });
